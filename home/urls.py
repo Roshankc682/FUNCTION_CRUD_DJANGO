e861788bb8c3234290a0ecd1a1581694a4a123d5
Roshan kc <kcroshan682@gmail.com>
2021-08-29 01:28:04 +0545

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from home.views import *
 
 urlpatterns = [
     path('books/create',book_create,name="books-create"),
-    path('books/<int:id>',book_update,name="books-update"),
+    path('books/<int:id>/update',book_update,name="books-update"),
     path('books/',book_list,name="books-list"),
     path('books/<int:id>/details',book_single,name="book-single"),
     path('books/<int:id>/delete',book_delete,name="book-delete"),
